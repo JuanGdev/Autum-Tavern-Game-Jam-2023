6be1c8fe12c0c8f3f6a08e27586f4b7e1d84a2c6
alexcavadora <49330757+alexcavadora@users.noreply.github.com>
2023-09-20 23:53:15 -0600

    Started work on movement class

--- Daredevil_character/daredevil.gd
+++ Daredevil_character/daredevil.gd
@@ -1,7 +1,6 @@
 extends CharacterBody2D
+class_name Daredevil
 
-
-var POINTS = 10
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
@@ -9,20 +8,24 @@ const JUMP_VELOCITY = -400.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var anim = get_node("AnimationPlayer")
-
+func _ready():
+	velocity = Vector2.ZERO
+	
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
 	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		anim.play("Jump")
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = 1
+	if(velocity.x < 0):
+		direction = -1
 	#print(direction)
 	
 	if direction == -1:
@@ -42,8 +45,4 @@ func _physics_process(delta):
 		anim.play("Fall")
 
 	move_and_slide()
-	
-	#if POINTS <= 0:
-		#queue_free()
-		#get_tree().change_scene_to_file("res://Scenes/main.tscn")
 
