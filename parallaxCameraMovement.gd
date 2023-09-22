e1bda66ae1b9798670cc5d0e17aa7153c2554e99
Isaac Salas <iscprepa@gmail.com>
2023-09-22 13:10:11 -0600

    Restored Parallax
    Restored Parallax scene and Sprites

---
+++ parallaxCameraMovement.gd
@@ -0,0 +1,8 @@
+extends CharacterBody2D
+@onready var playerCamera = $Camera2D
+
+func _physics_process(delta):
+	# Handle Jump.
+	if Input.is_action_pressed("jump"):
+		playerCamera.position.x += delta * 100
+		
