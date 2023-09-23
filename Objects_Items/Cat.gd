8e99850ab5818e45de629e8801b87365566f7f66
alexcavadora <49330757+alexcavadora@users.noreply.github.com>
2023-09-22 23:17:14 -0600

    Added most items, simple functions

---
+++ Objects_Items/Cat.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+@onready var timer = $Timer
+@onready var sprite = $Cat
+
+func _on_body_entered(body):
+	if body.name == "Daredevil":
+		Game.SCORECURRENT += 500
+		Game.ADREN -= 100
+		Game.SLOWED = true
+		Game.BOOSTED = false
+		sprite.hide()
+		timer.start()
+
+
+func _on_timer_timeout():
+	Game.BOOSTED = false
+	queue_free()
